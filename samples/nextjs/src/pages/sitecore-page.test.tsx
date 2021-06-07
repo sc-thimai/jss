657b3dc046824406366db97271911bc29db058a6
Oleksandr Kiselov <oleksandr.kiselov@sitecore.com>
2021-06-08 01:57:08 +0300

    SitecorePage tests are added

--- samples/nextjs/src/pages/sitecore-page.test.tsx
+++ samples/nextjs/src/pages/sitecore-page.test.tsx
@@ -1,41 +1,89 @@
 import 'jsdom-global/register';
-import { expect } from 'chai';
+import { expect, use } from 'chai';
+import sinonChai from 'sinon-chai';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 import { SitecorePageProps } from 'lib/page-props';
 import SitecorePage from './[[...path]]';
+import { layoutPersonalizationService } from 'lib/layout-personalization-service';
+
+use(sinonChai);
 
 describe('SitecorePage', () => {
   describe('useMemo', () => {
-    it('should return rendered component if it is not null', () => {
-      const SitecorePageComponent = () => {
-        SitecorePage({ ...getSitecorePageProps() });
+    beforeEach(function () {
+      sinon.spy(layoutPersonalizationService, 'loadPersonalization');
+    });
 
-        return <div>Loading</div>;
+    afterEach(function () {
+      sinon.restore();
+    });
+
+    it('should call loadPersonalization once with passed props', () => {
+      shallow(<SitecorePage {...sitecorePageProps} />);
+      expect(layoutPersonalizationService.loadPersonalization).to.have.been.callCount(1);
+    });
+
+    it('should return empty component rendering if application is running in disconnected mode', () => {
+      const props: SitecorePageProps = {
+        ...sitecorePageProps,
+        layoutData: {
+          sitecore: {
+            context: {},
+            route: {
+              name: 'name',
+              layoutId: 'available-in-connected-mode',
+              placeholders: { 'jss-main': [] },
+            },
+          },
+        },
       };
+      shallow(<SitecorePage {...props} />);
+      expect(layoutPersonalizationService.loadPersonalization).to.have.been.callCount(0);
+    });
 
-      const wrapper = shallow(<SitecorePageComponent />);
-      expect(wrapper).is.not.null;
-      expect(wrapper.html()).contains('Loading');
+    it('should return empty component rendering if application is running on server', () => {
+      global.window = undefined;
+      shallow(<SitecorePage {...sitecorePageProps} />);
+      expect(layoutPersonalizationService.loadPersonalization).to.have.been.callCount(0);
+    });
+
+    it('should return empty component rendering if application is running in preview mode', () => {
+      const props: SitecorePageProps = {
+        ...sitecorePageProps,
+        isPreview: true,
+      };
+      shallow(<SitecorePage {...props} />);
+      expect(layoutPersonalizationService.loadPersonalization).to.have.been.callCount(0);
     });
   });
 });
 
-function getSitecorePageProps(): SitecorePageProps {
-  return {
-    locale: '',
-    layoutData: {
-      sitecore: {
-        context: {},
-        route: {
-          name: 'name',
-          placeholders: { 'jss-main': [] },
-        },
+const sitecorePageProps: SitecorePageProps = {
+  locale: '',
+  layoutData: {
+    sitecore: {
+      context: {},
+      route: {
+        name: 'name',
+        placeholders: { 'jss-main': [] },
       },
     },
-    dictionary: {},
-    componentProps: {},
-    notFound: false,
-    tracked: false,
-    isPreview: false,
-  };
+  },
+  dictionary: {},
+  componentProps: {},
+  notFound: false,
+  tracked: false,
+  isPreview: false,
+};
+
+interface CustomWindow {
+  [key: string]: unknown;
+  document: unknown;
 }
+
+interface Global {
+  window: CustomWindow | undefined;
+}
+
+declare const global: Global;
